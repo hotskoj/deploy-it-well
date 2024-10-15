27608a229231e4397e4b9ab06ef291731ad098f9
Jessica H <jessianne001@gmail.com>
2024-10-15 18:26:46 -0400

    Add another test for h2

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says tests pass', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Tests Pass');
+        done();
+      });
+    });
 })
